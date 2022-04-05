e0611e335ec2a02ebd9b8cd863129bacd69887c7
carharv <61243088+carharv@users.noreply.github.com>
2022-04-04 21:17:55 -0400

    Deleted HelloWorld.vue and added Map.vue

---
+++ src/components/Map.vue
@@ -0,0 +1,29 @@
+<template>
+  <LMap
+    style="height: 500px; width: 800px"
+    :zoom="13"
+    :center="mapCenter"
+    @click="onMapClicked"
+  >
+    <LTileLayer :url="mapUrl" :attribution="mapAttribution"></LTileLayer>
+  </LMap>
+</template>
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+import "leaflet/dist/leaflet.css";
+
+@Component({ components: { LMap, LTileLayer, LMarker } })
+export default class WorldMap extends Vue {
+  geoPos: { lat?: number; lng?: number } = {};
+  mapCenter = [42.963, -85.668];
+  mapUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+  mapAttribution =
+    "&copy; <a target='_blank' href='http://osm.org/copyright'>OpenStreetMap</a>";
+
+  onMapClicked(e: any): void {
+    // Emit an event to notify the parent element
+    this.$emit("map-clicked", e.latlng);
+  }
+}
+</script>
