0f20eac09d3ee63944cf652eedad2044c5bcf6fb
carharv <61243088+carharv@users.noreply.github.com>
2022-04-18 15:06:55 -0400

    resolve conflicts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,12 @@
   <div>
     <nav>
       <router-link to="/home">Home</router-link>
-    </nav>
     <h1>This heading is from HomeView.vue</h1>
   </div>
 </template>
 
 <script>
+import { Vue } from "vue-property-decorator";
 import { Vue } from "vue-property-decorator";
 
 export default class HomeView extends Vue {}
