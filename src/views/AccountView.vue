b1f6168a3760fd0f1ea9b6bd313ec93ef86f0207
carharv <61243088+carharv@users.noreply.github.com>
2022-04-20 13:47:19 -0400

    fix UID bug in CreateAccount

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -9,7 +9,7 @@
     <div id="accountTabs">
       <b-tabs pills vertical>
         <b-tab title="Test" active>
-          <b-card-text>Test Contents</b-card-text>
+          <b-card-text>{{ uid }}</b-card-text>
         </b-tab>
         <b-tab title="Test2"
           ><b-card-text>{{ testVar }}</b-card-text></b-tab
@@ -22,10 +22,44 @@
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 import { BTab, BTabs } from "bootstrap-vue";
+import { app } from "../firebaseConfig";
+import {
+  getAuth,
+  onAuthStateChanged,
+  User,
+  Auth,
+  signOut,
+  deleteUser,
+} from "firebase/auth";
+import {
+  collection,
+  CollectionReference,
+  doc,
+  DocumentReference,
+  Firestore,
+  DocumentSnapshot,
+  getDoc,
+  getFirestore,
+  setDoc,
+  deleteDoc,
+} from "firebase/firestore";
+
+//Constants
+const db: Firestore = getFirestore(app);
+const userColl: CollectionReference = collection(db, "users");
 
 @Component({ components: { BTab, BTabs } })
 export default class AccountView extends Vue {
   testVar = "This is from the testVar";
+  uid: string | undefined = "";
+  userDoc!: DocumentReference;
+  auth: Auth | null = null;
+
+  mounted() {
+    this.auth = getAuth();
+    this.uid = this.auth?.currentUser?.uid;
+    this.userDoc = doc(userColl, this.uid);
+  }
 }
 </script>
 
