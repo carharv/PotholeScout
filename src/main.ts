0c16c9de29393c0397784f8c81755daceefb0f87
carharv <61243088+carharv@users.noreply.github.com>
2022-04-22 13:15:15 -0400

    resolve conflict

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,14 @@
-//Imports
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import SmartTable from "vuejs-smart-table";
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./firebaseConfig";
-import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
-import "leaflet/dist/leaflet.css";
 
-//Use
-Vue.use(BootstrapVue);
-Vue.use(BootstrapVueIcons);
 Vue.use(SmartTable);
 
 Vue.config.productionTip = false;
+
 const app = initializeApp(firebaseConfig);
 
 new Vue({
